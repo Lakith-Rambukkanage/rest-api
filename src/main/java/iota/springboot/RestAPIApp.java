7d559860a341ea59ed9bf823ef10585c7e131e67
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2020-10-08 17:59:01 +0530

    Initial commit

---
+++ src/main/java/iota/springboot/RestAPIApp.java
@@ -0,0 +1,11 @@
+package iota.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  RestAPIApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(RestAPIApp.class,args);
+    }
+}
