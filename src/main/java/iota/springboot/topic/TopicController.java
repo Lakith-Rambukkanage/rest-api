b6f0c245df4c6fa2b6c353ece56ba90c26c0f868
“Lakith <lakith.rambukkanage@avtra.com>
2020-10-12 17:52:56 +0530

    Rest Api wiht get put post delete

--- src/main/java/iota/springboot/topic/TopicController.java
+++ src/main/java/iota/springboot/topic/TopicController.java
@@ -1,23 +1,44 @@
 package iota.springboot.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topic")
     public List<Topic> getAllTopics(){
         //get converted to JSON by spring MVC
-        return Arrays.asList(
-                new Topic("Spring","Spring Framework","About Spring Framework"),
-                new Topic("java","Java Lang.","About Java"),
-                new Topic("js","JavaScript","About JS")
-        );
+        return topicService.getAllTopics();
     }
 
+    @RequestMapping("/topic/{id}")
+    public Topic getTopicById(@PathVariable String id){
+        return topicService.getTopicById(id);
+    }
+
+    @RequestMapping(value = "/topic",method = RequestMethod.POST)
+    public void addTopic(@RequestBody Topic topic){
+        //spring takes care of the body to object conversion @RequestBody
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(value = "/topic/{id}",method = RequestMethod.PUT)
+    public void updateTopic(@PathVariable String id,@RequestBody Topic topic){
+        topicService.updateTopic(topic,id);
+    }
+
+    @RequestMapping(value = "/topic/{id}",method = RequestMethod.DELETE)
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
 }
